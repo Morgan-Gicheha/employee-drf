bfdf25c0fedbb8aecdf404bb0772b2c6a09c6420
morgan-gicheha <morgangicheha4@gmail.com>
2022-06-10 13:29:44 +0300

    .

--- employeeApp/settings.py
+++ employeeApp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     "corsheaders",
+   "corsheaders",
     'employeeApp',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
         "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+ 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
