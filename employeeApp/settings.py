8d0d92ee062d040bebd399079f3e8993a06aff8b
morgan-gicheha <morgangicheha4@gmail.com>
2022-06-10 13:35:49 +0300

    cors

--- employeeApp/settings.py
+++ employeeApp/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
-# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 
 
 # CORS_ALLOWED_ORIGINS = [
